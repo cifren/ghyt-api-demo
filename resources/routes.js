d8821faeeb3cc40ffdc8fdef9ff813af74405e2c
Francis <cifren@msn.com>
2020-02-07 17:46:53 +0100

    Add new vuejs component

--- resources/routes.js
+++ resources/routes.js
@@ -1,16 +1,35 @@
 import VueRouter from 'vue-router';
 import Home from './views/Home.vue'
-import Jobs from './views/Jobs.vue'
+import Jobs from './views/job/Jobs.vue'
+import JobForm from './views/job/JobForm.vue'
+import ActionForm from './views/job/ActionForm.vue'
+import ConditionForm from './views/job/ConditionForm.vue'
 import Logs from './views/Logs.vue'
 
 let routes = [
   {
     path: '/',
     component: Home
-  },  {
+  },
+  {
     path: '/jobs',
-    component: Jobs
-  },  {
+    component: Jobs,
+  },
+  {
+    path: '/job',
+    component: JobForm,
+    children: [
+      {
+        path: '/action/:id',
+        component: ActionForm
+      },
+      {
+        path: '/condition/:id',
+        component: ConditionForm
+      }
+    ]
+  },
+  {
     path: '/logs',
     component: Logs
   }
---
+++ resources/views/job/JobForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <b-button @click="addNewCondition">Add Condition</b-button>
+    <div v-for="condition in conditions">
+      <ConditionForm
+        :condition.sync="condition.name"
+      ></ConditionForm>
+    </div>
+  </div>
+</template>
+
+<script>
+  import ConditionForm from '../../views/job/ConditionForm.vue'
+
+  export default {
+    name: 'jobForm',
+    components: {
+      ConditionForm
+    },
+    data: function() {
+      return {
+        conditions: []
+      };
+    },
+    methods: {
+      addNewCondition() {
+        let condition = {"name": "plop", "arguments": "lol"};
+        this.conditions.push(condition)
+      },
+      addNewAction() {
+
+      }
+    }
+  }
+</script>
