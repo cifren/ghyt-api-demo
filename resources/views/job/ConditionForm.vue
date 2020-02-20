1a615947e637b222d1689bccd5ec3aa83a4f015a
Francis <cifren@msn.com>
2020-02-20 18:49:35 +0100

    Fix form

--- resources/views/job/ConditionForm.vue
+++ resources/views/job/ConditionForm.vue
@@ -80,7 +80,7 @@
             <b-input
               v-bind:value="key"
               placeholder="Key"
-              @input="validKey(key, $event)?$emit('update:args', updateArgKey(args, key, $event)):false"></b-input>
+              @blur="validKey(key, $event.target.value)?$emit('update:args', updateArgKey(args, key, $event.target.value)):false"></b-input>
             <b-button
               class="is-static has-text-primary"
               icon-right="angle-double-right"></b-button>
