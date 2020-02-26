3e069c56ac9438472d463626eded3d4eaf1b63aa
Francis <cifren@msn.com>
2020-02-26 14:14:16 +0100

    npm: Improve date display

--- resources/views/log/LogPreview.vue
+++ resources/views/log/LogPreview.vue
@@ -5,7 +5,7 @@
         <div class="media-content">
           <p class="title is-capitalized">
             <b-icon icon="briefcase" size="is-large" type="is-primary"/>
-            {{created_at | moment("YYYY/MM/DD h:mm:ss")}}
+            {{created_at | moment("YYYY/MM/DD HH:mm:ss")}}
           </p>
         </div>
       </div>
